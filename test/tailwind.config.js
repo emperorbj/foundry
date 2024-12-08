c5b026c4ee6eaf3e36ee1ee89a4a5f857a3f8478
emperorbj <cwaku96@gmail.com>
2024-12-09 00:21:05 +0100

    `Update App.jsx to use a div instead of a fragment and update tailwind.config.js to use tailwindcssAnimate plugin`

--- test/tailwind.config.js
+++ test/tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindcssAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -80,6 +81,6 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
 
